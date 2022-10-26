3413edbd2ba111417f676189f30ad9a903451fd3
Dhruv Gupta <112793698+DhruvGupta2004@users.noreply.github.com>
2022-10-26 12:44:14 +0530

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -1,34 +1,45 @@
 package pattern;
 
 import java.util.*;
+
 public class Diamond {
+    
     public static void main(String[]arg) {
         try(Scanner sc = new Scanner(System.in)){
             System.out.println("rows");
             int n = sc.nextInt();
+            
+            
             //upper
             for(int i=1;i<=n;i++){
+                
                 //spaces
                 for(int j=1;j<=(n-i);j++){
                     System.out.print(" ");
                 }
+                
                 //1 half
                 for(int j=1;j<=2*i-1;j++){
                     System.out.print("*");
                 }
                 System.out.println(); 
             }
+            
             //lower
+            
             for(int i=n;i>=1;i--){
+                
                 //spaces
                 for(int j=1;j<=(n-i);j++){
                     System.out.print(" ");
                 }
-                //stars
+                
+                //star
                 for(int j=1;j<=2*i-1;j++){
                     System.out.print("*");
                 }
                 System.out.println();
+                
             }    
         }
     }
