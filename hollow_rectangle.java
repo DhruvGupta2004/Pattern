e09e34ce12d64f72a0c51124fe6d5c6e95cb8927
Dhruv Gupta <112793698+DhruvGupta2004@users.noreply.github.com>
2022-10-27 11:16:06 +0530

    Update hollow_rectangle.java

--- hollow_rectangle.java
+++ hollow_rectangle.java
@@ -1,18 +1,24 @@
 package pattern;
 
 import java.util.*;
+
 public class hollow_rectangle {
     public static void main(String[]arg) {
+        
         try(Scanner sc = new Scanner(System.in)){
+            
             System.out.println("rows");
             int rows = sc.nextInt();
+            
             System.out.println("coloumn");
             int col = sc.nextInt();
+            
             for(int i=1;i<=rows;i++){
                 for(int j=1;j<=col;j++){
                     if(i==1 || j==1 || i==rows || j==col){
                         System.out.print("*");
                     }
+                    
                     else{
                         System.out.print(" ");
                     }
